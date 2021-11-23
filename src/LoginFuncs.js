866ac01dc5077c9dd6d250d8474ef7f52ec6fd9d
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-11-23 09:43:43 +0900

    [UPDATE] 토큰 갱신할 수 있도록 설정

--- src/LoginFuncs.js
+++ src/LoginFuncs.js
@@ -5,13 +5,28 @@ export const getAccessToken = async (code) => {
     method: "GET",
     url: `/api/getAccessToken?code=${code}`,
   });
-  return res.data.ACCESS_TOKEN;
+  return {
+    accessToken: res.data.ACCESS_TOKEN,
+    refreshToken: res.data.REFRESH_TOKEN,
+  };
+};
+
+export const refreshAccessToken = async (refreshToken) => {
+  const res = await instance({
+    method: "POST",
+    url: `/api/refreshAccessToken`,
+    data: { REFRESH_TOKEN: refreshToken },
+  });
+  return {
+    accessToken: res.data.ACCESS_TOKEN,
+  };
 };
 
 export const getUserInfo = async (accessToken) => {
   const res = await instance({
-    method: "GET",
+    method: "POST",
     url: `/api/getUserInfo?ACCESS_TOKEN=${accessToken}`,
+    data: { ACCESS_TOKEN: accessToken },
   });
   return res.data;
 };
--- src/components/Pages/LoginingPage.js
+++ src/components/Pages/LoginingPage.js
@@ -1,5 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { getAccessToken, getUserInfo, loginUser } from "../../LoginFuncs";
+import {
+  getAccessToken,
+  getUserInfo,
+  loginUser,
+  refreshAccessToken,
+} from "../../LoginFuncs";
 import { useUserDispatch } from "../Contexts/UserContext";
 import LoginingPageTemplate from "../Templates/LoginingPageTemplate";
 
@@ -9,22 +14,59 @@ export default function LoginingPage({ setNowPage }) {
   const getUserInfoFirstCnt = useRef(0);
   const loginUserFirstCnt = useRef(0);
   const userDispatch = useUserDispatch();
+  const storedAccessToken = localStorage.getItem("accessToken");
+  const storedRefreshToken = localStorage.getItem("refreshToken");
+
+  const goBackToLoginPage = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    setNowPage("login");
+  };
+
+  const _getAccessToken = () => {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get("code");
+    getAccessToken(code)
+      .then(({ accessToken, refreshToken }) => {
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+        setAccessToken(accessToken);
+      })
+      .catch((err) => {
+        console.error("액세스 토큰을 받아오는 도중 오류가 발생했습니다.");
+        goBackToLoginPage();
+      });
+  };
+
+  const _refreshAccessToken = () => {
+    refreshAccessToken(storedRefreshToken)
+      .then(({ accessToken }) => {
+        localStorage.setItem("accessToken", accessToken);
+        setAccessToken(accessToken);
+      })
+      .catch((err) => {
+        console.error("액세스 토큰을 받아오는 도중 오류가 발생했습니다.");
+        goBackToLoginPage();
+      });
+  };
+
+  const _getUserInfo = () => {
+    getUserInfo(accessToken)
+      .then((data) => setUserInfo(data))
+      .catch((err) => {
+        const errStatus = err.response.status;
+        if (errStatus === 422) {
+          _refreshAccessToken();
+        } else {
+          console.error("사용자 정보를 받아오는 도중 오류가 발생했습니다.");
+          goBackToLoginPage();
+        }
+      });
+  };
 
   useEffect(() => {
-    const storedAccessToken = localStorage.getItem("accessToken");
     if (!storedAccessToken) {
-      const url = new URL(window.location.href);
-      const code = url.searchParams.get("code");
-      getAccessToken(code)
-        .then((accessToken) => {
-          localStorage.setItem("accessToken", accessToken);
-          setAccessToken(accessToken);
-        })
-        .catch((err) => {
-          console.error("액세스 토큰을 받아오는 도중 오류가 발생했습니다.");
-          localStorage.removeItem("accessToken");
-          setNowPage("login");
-        });
+      _getAccessToken();
     } else {
       setAccessToken(storedAccessToken);
     }
@@ -35,15 +77,10 @@ export default function LoginingPage({ setNowPage }) {
     if (getUserInfoFirstCnt.current <= 0) {
       getUserInfoFirstCnt.current = 1;
     } else {
-      getUserInfo(accessToken)
-        .then((data) => setUserInfo(data))
-        .catch((err) => {
-          console.error("사용자 정보를 받아오는 도중 오류가 발생했습니다.");
-          localStorage.removeItem("accessToken");
-          setNowPage("login");
-        });
+      _getUserInfo();
     }
-  }, [accessToken, setNowPage]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accessToken]);
 
   useEffect(() => {
     if (loginUserFirstCnt.current <= 0) {
